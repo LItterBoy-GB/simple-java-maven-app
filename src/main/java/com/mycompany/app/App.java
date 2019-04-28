d120269d4fa58b96c1120b6b11bce92b7e0c62e0
JavaLitterBoy <javalitterboy@gmail.com>
2019-04-28 11:01:02 +0800

    测试数据库

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,10 @@
 package com.mycompany.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  */
@@ -10,8 +15,16 @@ public class App
 
     public App() {}
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         System.out.println(new App().getMessage());
+        // 1.加载数据访问驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        //2.连接到数据"库"上去
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ibk", "ibk", "i123b456k");
+            //3.构建SQL命令
+            Statement state=conn.createStatement();
+            state.executeQuery("select * from user;");
+            conn.close();
     }
 
     private final String getMessage() {
