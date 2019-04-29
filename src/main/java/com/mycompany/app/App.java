b249a1302278e87992aea218167813eef0cbe71b
JavaLitterBoy <javalitterboy@gmail.com>
2019-04-29 14:55:12 +0800

    修改超时时间

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,7 +17,7 @@ public class App
         // 1.加载数据访问驱动
         Class.forName("com.mysql.jdbc.Driver");
         //2.连接到数据"库"上去
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ibk", "ibk", "i123b456k");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ibk?connectTimeout=6000&socketTimeout=60000", "ibk", "i123b456k");
         //3.构建SQL命令
         Statement state=conn.createStatement();
         ResultSet res =state.executeQuery("select * from user;");
